d8d18bddac44b2f16827eee4bdc1a1ea9975d389
MomchilAngelov <momodota2@gmail.com>
2015-10-27 08:15:11 +0200

    Added the info class

---
+++ Projects_Java/Tanev_03/src/lettrywhatTanevwanted/Info.java
@@ -0,0 +1,8 @@
+package lettrywhatTanevwanted;
+
+public class Info extends Basic_Calculator{
+	public int execute(String[] args){
+		System.out.println("SUpported operations: Addition, Substraction, Division, Multiplication");
+		return 0;
+	}
+}
